8a01e99dfed30b43b26d241f526915d438ffbd35
ajonczyk <01178532@pw.edu.pl>
2024-04-08 13:04:48 +0200

    Implemented GameMap, Obstacle and PowerUp class

--- core/src/com/avalancherush/game/Models/Obstacle.java
+++ core/src/com/avalancherush/game/Models/Obstacle.java
@@ -1,4 +1,41 @@
 package com.avalancherush.game.Models;
 
-public class Obstacle {
+import com.avalancherush.game.Interfaces.Collidable;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+
+public class Obstacle extends Collidable {
+
+    private int type;
+
+    private int track; // new thing - on which track is this obstacle
+
+    private Image image;
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public int getTrack() {
+        return track;
+    }
+
+    public void setTrack(int track) throws Exception {
+        if (track >= 1 && track <= 5) {
+            this.track = track;
+        } else {
+            throw new Exception("Invalid track number. Track number must be between 1 and 5.");
+        }
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
 }
