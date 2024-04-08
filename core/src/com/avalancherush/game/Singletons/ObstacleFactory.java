270ec22a38a5c9150fa693adbb7eac7b13c71679
ZZZdream <kmultan1234@gmail.com>
2024-04-08 16:48:47 +0200

    Implement createObstacle function in ObstacleFactory

--- core/src/com/avalancherush/game/Singletons/ObstacleFactory.java
+++ core/src/com/avalancherush/game/Singletons/ObstacleFactory.java
@@ -1,5 +1,9 @@
 package com.avalancherush.game.Singletons;
 
+import com.avalancherush.game.Enums.ObstacleType;
+import com.avalancherush.game.Models.Obstacle;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+
 public class ObstacleFactory {
     private static ObstacleFactory instance;
     public ObstacleFactory getInstance(){
@@ -7,5 +11,26 @@ public class ObstacleFactory {
             instance = new ObstacleFactory();
         }
         return instance;
+    }
+
+    public Obstacle createObstacle(ObstacleType obstacleType, int track){
+        Obstacle obstacle = new Obstacle();
+        try{
+            obstacle.setTrack(track);
+        }catch (Exception e){
+            System.out.println(e);
+        }
+        if(obstacleType == ObstacleType.ROCK){
+            Image image = new Image();
+            obstacle.setImage(image);
+            obstacle.setType(ObstacleType.ROCK);
+            obstacle.setJumpable(true);
+        }else if (obstacleType == ObstacleType.TREE){
+            Image image = new Image();
+            obstacle.setImage(image);
+            obstacle.setType(ObstacleType.TREE);
+            obstacle.setJumpable(false);
+        }
+        return obstacle;
     }
 }
