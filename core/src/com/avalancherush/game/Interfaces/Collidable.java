0e0f7d100cff880859892c950149789b9d996e12
ZZZdream <kmultan1234@gmail.com>
2024-04-08 12:26:27 +0200

    Change Drawable interface to abstract class and pass Rectancle property from Collidable to Drawable

--- core/src/com/avalancherush/game/Interfaces/Collidable.java
+++ core/src/com/avalancherush/game/Interfaces/Collidable.java
@@ -3,14 +3,15 @@ package com.avalancherush.game.Interfaces;
 
 import com.badlogic.gdx.math.Rectangle;
 
-public abstract class Collidable implements Drawable {
-    private Rectangle rectangle;
-    boolean collides(Rectangle rectangle){
+public abstract class Collidable extends Drawable {
+    public boolean collides(Rectangle rectangle){
         if(this.rectangle.overlaps(rectangle)){
             return true;
         }
         return false;
     };
+
+
 
 
 }
--- core/src/com/avalancherush/game/Interfaces/Drawable.java
+++ core/src/com/avalancherush/game/Interfaces/Drawable.java
@@ -1,8 +1,15 @@
 package com.avalancherush.game.Interfaces;
 
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Rectangle;
 
-public interface Drawable {
-    void draw();
-    void update();
+public abstract class Drawable {
+    protected Rectangle rectangle;
+    public void draw(SpriteBatch spriteBatch, TextureRegion textureRegion) {
+        spriteBatch.draw(textureRegion, rectangle.x, rectangle.y, rectangle.width, rectangle.height);
+    }
+//    void update(){
+//
+//    };
 }
