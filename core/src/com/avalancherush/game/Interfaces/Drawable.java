90816800b9138a0d87dc899deb0e218feee24a03
ZZZdream <kmultan1234@gmail.com>
2024-04-09 17:35:27 +0200

    Add Texture field to Drawable abstract class

--- core/src/com/avalancherush/game/Interfaces/Drawable.java
+++ core/src/com/avalancherush/game/Interfaces/Drawable.java
@@ -1,15 +1,32 @@
 package com.avalancherush.game.Interfaces;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Rectangle;
 
 public abstract class Drawable {
     protected Rectangle rectangle;
-    public void draw(SpriteBatch spriteBatch, TextureRegion textureRegion) {
-        spriteBatch.draw(textureRegion, rectangle.x, rectangle.y, rectangle.width, rectangle.height);
+    protected Texture texture;
+    public void draw(SpriteBatch spriteBatch) {
+        spriteBatch.draw(texture, rectangle.x, rectangle.y, rectangle.width, rectangle.height);
     }
-//    void update(){
+
+    public Rectangle getRectangle() {
+        return rectangle;
+    }
+
+    public void setRectangle(Rectangle rectangle) {
+        this.rectangle = rectangle;
+    }
+
+    public Texture getTexture() {
+        return texture;
+    }
+
+    public void setTexture(Texture texture) {
+        this.texture = texture;
+    }
+    //    void update(){
 //
 //    };
 }
