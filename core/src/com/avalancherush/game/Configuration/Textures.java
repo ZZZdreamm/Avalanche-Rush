61be76e07c27e7e6dabd516e87246eb376b8bafa
ZZZdream <kmultan1234@gmail.com>
2024-04-09 10:21:00 +0200

    Create Global Textures class with static textures

---
+++ core/src/com/avalancherush/game/Configuration/Textures.java
@@ -0,0 +1,11 @@
+package com.avalancherush.game.Configuration;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+
+public class Textures {
+    public static Texture scoreboard = new Texture((Gdx.files.internal("buttonWood.png")));
+    public static Texture rock = new Texture((Gdx.files.internal("rock.png")));
+    public static Texture tree = new Texture((Gdx.files.internal("treewinter.png")));
+    public static Texture line = new Texture((Gdx.files.internal("Line.png")));
+}
