c8b8e470129ea43dc263710ce28e14bc12dc127d
ZZZdream <kmultan1234@gmail.com>
2024-04-09 09:02:19 +0200

    Add GlobalVariables class with fixed static values

---
+++ core/src/com/avalancherush/game/Configuration/GlobalVariables.java
@@ -0,0 +1,6 @@
+package com.avalancherush.game.Configuration;
+
+public class GlobalVariables {
+    public static float SINGLE_PLAYER_HEIGHT = 300;
+    public static float SINGLE_PLAYER_WIDTH = 300;
+}
