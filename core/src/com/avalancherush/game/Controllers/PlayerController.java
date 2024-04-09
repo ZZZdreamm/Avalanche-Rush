8653f3c4809ccba4b8da9eb042542fd4a4aa2c0b
ZZZdream <kmultan1234@gmail.com>
2024-04-09 17:37:18 +0200

    Setup PlayerController as EventObserver

--- core/src/com/avalancherush/game/Controllers/PlayerController.java
+++ core/src/com/avalancherush/game/Controllers/PlayerController.java
@@ -1,4 +1,32 @@
 package com.avalancherush.game.Controllers;
 
-public class PlayerController {
+import com.avalancherush.game.Enums.EventType;
+import com.avalancherush.game.Interfaces.EventObserver;
+import com.avalancherush.game.Models.Player;
+
+import java.util.List;
+
+public class PlayerController implements EventObserver {
+    private List<Player> players;
+    @Override
+    public void notify(EventType eventType) {
+        switch (eventType){
+            case SLIDED_UP: {
+                break;
+            }
+            case SLIDED_LEFT: {
+
+            }
+            case SLIDED_RIGHT: {
+
+            }
+        }
+    }
+
+    public void setPlayers(List<Player> players) {
+        this.players = players;
+    }
+    public List<Player> getPlayers() {
+        return players;
+    }
 }
