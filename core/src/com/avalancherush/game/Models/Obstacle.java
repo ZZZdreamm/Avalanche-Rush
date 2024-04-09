177285f978cce487c74df92fa7ecb68955b3cd22
ZZZdream <kmultan1234@gmail.com>
2024-04-09 17:36:08 +0200

    Remove Image from Obstacle and use Rectangle and Texture in creation of Obstacles in ObstacleFactory

--- core/src/com/avalancherush/game/Models/Obstacle.java
+++ core/src/com/avalancherush/game/Models/Obstacle.java
@@ -7,7 +7,6 @@ import com.badlogic.gdx.scenes.scene2d.ui.Image;
 public class Obstacle extends Collidable {
     private ObstacleType type;
     private int track; // new thing - on which track is this obstacle
-    private Image image;
     private boolean jumpable;
 
     public Obstacle(){
@@ -31,14 +30,6 @@ public class Obstacle extends Collidable {
         } else {
             throw new Exception("Invalid track number. Track number must be between 1 and 5.");
         }
-    }
-
-    public Image getImage() {
-        return image;
-    }
-
-    public void setImage(Image image) {
-        this.image = image;
     }
 
     public boolean getJumpable(){
--- core/src/com/avalancherush/game/Singletons/ObstacleFactory.java
+++ core/src/com/avalancherush/game/Singletons/ObstacleFactory.java
@@ -1,19 +1,25 @@
 package com.avalancherush.game.Singletons;
 
+import static com.avalancherush.game.Configuration.GlobalVariables.OBSTACLE_HEIGHT;
+import static com.avalancherush.game.Configuration.GlobalVariables.OBSTACLE_ROCK_WIDTH;
+import static com.avalancherush.game.Configuration.GlobalVariables.OBSTACLE_TREE_WIDTH;
+
+import com.avalancherush.game.Configuration.Textures;
 import com.avalancherush.game.Enums.ObstacleType;
 import com.avalancherush.game.Models.Obstacle;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 
 public class ObstacleFactory {
     private static ObstacleFactory instance;
-    public ObstacleFactory getInstance(){
+    public static ObstacleFactory getInstance(){
         if(instance == null){
             instance = new ObstacleFactory();
         }
         return instance;
     }
 
-    public Obstacle createObstacle(ObstacleType obstacleType, int track){
+    public Obstacle createObstacle(ObstacleType obstacleType, int track, float x, float y){
         Obstacle obstacle = new Obstacle();
         try{
             obstacle.setTrack(track);
@@ -21,15 +27,17 @@ public class ObstacleFactory {
             System.out.println(e);
         }
         if(obstacleType == ObstacleType.ROCK){
-            Image image = new Image();
-            obstacle.setImage(image);
             obstacle.setType(ObstacleType.ROCK);
             obstacle.setJumpable(true);
+            Rectangle rectangle = new Rectangle(x, y, OBSTACLE_ROCK_WIDTH, OBSTACLE_HEIGHT);
+            obstacle.setRectangle(rectangle);
+            obstacle.setTexture(Textures.ROCK);
         }else if (obstacleType == ObstacleType.TREE){
-            Image image = new Image();
-            obstacle.setImage(image);
             obstacle.setType(ObstacleType.TREE);
             obstacle.setJumpable(false);
+            Rectangle rectangle = new Rectangle(x, y, OBSTACLE_TREE_WIDTH, OBSTACLE_HEIGHT);
+            obstacle.setRectangle(rectangle);
+            obstacle.setTexture(Textures.TREE);
         }
         return obstacle;
     }
