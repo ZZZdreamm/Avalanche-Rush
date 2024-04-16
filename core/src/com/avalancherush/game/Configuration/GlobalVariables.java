13e253be3fa6a9310979b3a8a5970a458b4d8d78
PandaMoyashi <margaux.xu@yahoo.fr>
2024-04-16 10:32:05 +0200

    Added resizing

--- core/src/com/avalancherush/game/Configuration/GlobalVariables.java
+++ core/src/com/avalancherush/game/Configuration/GlobalVariables.java
@@ -1,5 +1,7 @@
 package com.avalancherush.game.Configuration;
 
+import com.badlogic.gdx.Gdx;
+
 public class GlobalVariables {
     public static float SINGLE_PLAYER_HEIGHT = 110;
     public static float SINGLE_PLAYER_WIDTH = 70;
@@ -11,5 +13,14 @@ public class GlobalVariables {
     public static float POWER_UP_HEIGHT = 40;
     public static float POWER_UP_WIDTH = 40;
     public static float[] LANES = new float[3];
+    public static final float BASE_WIDTH = 480;
+    public static final float BASE_HEIGHT = 820;
+    public static float widthScale;
+    public static float heightScale;
+
+    public static void calculateScales() {
+        widthScale = Gdx.graphics.getWidth() / BASE_WIDTH;
+        heightScale = Gdx.graphics.getHeight() / BASE_HEIGHT;
+    }
 
 }
